b8340f5db24fa0db2d21678af1fef5f854c36d88
xiaozm <2731803833@qq.com>
2020-09-29 09:34:44 +0800

    cc

--- feichengwurao/src/main/java/com/xiao/RunAppFCWR.java
+++ feichengwurao/src/main/java/com/xiao/RunAppFCWR.java
@@ -1,7 +1,7 @@
 package com.xiao;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
